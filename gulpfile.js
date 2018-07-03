886ad217f1ebe8959bc137438f0c975ceda73305
iMervinC <itorma.mervin@gmail.com>
2018-07-03 18:00:17 +0800

    designing begins

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function(){
 	console.log("huray");
@@ -15,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('style', function(){
 	return gulp.src('./app/assets/style/style.css')
-	.pipe(postcss([ cssvars, nested, autoprefixer]))
+	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 	.pipe(gulp.dest('./app/temp/style'));
 });
 
