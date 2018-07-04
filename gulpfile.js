fda94c42b53128a4f9b6f34f49f57f0ec43b4292
iMervinC <itorma.mervin@gmail.com>
2018-07-04 21:09:51 +0800

    resuable blocks

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,4 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-browserSync = require('browser-sync').create();
+require('./gulp/task/style');
+require('./gulp/task/watch');
 
-gulp.task('default', function(){
-	console.log("huray");
-});
-
-gulp.task('html', function(){
-	console.log("Imagine something useful");
-});
-
-gulp.task('style', function(){
-	return gulp.src('./app/assets/style/style.css')
-	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-	.pipe(gulp.dest('./app/temp/style'));
-});
-
-gulp.task('watch', function () {
-
-	browserSync.init({
-		notify:false,
-		server: {
-			baseDir: "app"
-		}
-	});
-
-	watch('./app/index.html', function(){
-		browserSync.reload();
-	});
-
-	watch('./app/assets/style/**/*.css', function(){
-		gulp.start('cssInject');
-	});
-
-});
-
-gulp.task('cssInject', ['style'], function() {
-	return gulp.src('./app/temp/style/style.css')
-	.pipe(browserSync.stream());
-})
+	
